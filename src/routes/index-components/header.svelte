bafd94da87d2ddfed85b9f758b542eadf4af4fe8
Yudi <12040397+Yudi@users.noreply.github.com>
2023-09-20 19:01:38 -0300

    Adicionar botão de inscrição

--- src/routes/index-components/header.svelte
+++ src/routes/index-components/header.svelte
@@ -7,16 +7,17 @@
 
 <header class="header-bg">
   <div class="container">
-    <div class="text-center intro-text">
+    <div class="intro-text text-center">
       <div class="intro-lead-in intro-lead-in-padding">
         <img src={logo} width="100%" class="secompp-logo" alt="Logo SECOMPP 2023" />
       </div>
-      <div class="text-center intro-heading">
+      <div class="intro-heading text-center">
         <span class="fs-3 fw-semibold"> 20ª Semana do Curso de Ciência da Computação da FCT-Unesp </span>
       </div>
-      <div class="text-center intro-heading">
+      <div class="intro-heading text-center">
         <span class="fs-4 fw-light">Junte-se a nós de 25 a 29 de setembro de 2023</span>
       </div>
+      <Button outline class="my-3" size="lg" href="https://fct-pp.web.app/eventos">Inscrever-se</Button>
       <br />
       <br />
       Apoio:
