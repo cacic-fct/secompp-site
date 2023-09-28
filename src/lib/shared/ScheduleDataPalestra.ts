2523ba938adc54749a531deca55195d707f3f553
Willian Murayama <willianyos13@gmail.com>
2023-09-28 18:43:27 -0300

    Fix: nome dos membros da mesa redonda

--- src/lib/shared/ScheduleDataPalestra.ts
+++ src/lib/shared/ScheduleDataPalestra.ts
@@ -36,7 +36,7 @@ const ScheduleData: ScheduleEventList = [
     150,
     'Mesa-redonda',
     'Carreiras e trajetórias',
-    [{ name: 'Caio Takeshi Onishi' }, { name: 'Giulia Campos de Oliveira' }, { name: 'Leandro Ungari Cayres' },{name: 'William da Silva Lopes'}]
+    [{ name: 'Caio Takeshi Onishi' }, { name: 'Giulia Campos de Oliveira' }, { name: 'Leandro Ungari Cayres' },{name: 'William da Silva Lopes'},{name: 'Jaqueline Teixeira'}]
   ),
 ];
 
