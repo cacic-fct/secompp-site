ca1f727814442ba953176838ea304e91f513e584
Willian Murayama <willianyos13@gmail.com>
2023-12-07 10:17:25 -0300

    Correção dos membros da mesa redonda (#10)
    Co-authored-by: Yudi <12040397+Yudi@users.noreply.github.com>

--- src/routes/index-components/Schedule/Ending.svelte
+++ src/routes/index-components/Schedule/Ending.svelte
@@ -8,12 +8,11 @@
     <hr class="my-5" />
     <br />
     <!-- O evento chegou ao fim. Agradecemos a todos que participaram!<br /> -->
-    Você pode acessar os materiais utilizados durante os minicursos aqui:<br />
+    Você pode acessar os materiais utilizados durante os minicursos e palestras aqui:<br />
     <Button
       outline
       class="my-3"
       size="lg"
-      href="https://drive.google.com/drive/folders/1QS1X21c6kdrtTGymhBX-mMp7_AciSq1X"
-      >Acessar o material</Button>
+      href="https://drive.google.com/drive/folders/1QS1X21c6kdrtTGymhBX-mMp7_AciSq1X">Acessar o material</Button>
   </div>
 </div>
