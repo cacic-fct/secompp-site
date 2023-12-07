6ba38464316845cbc6e89783ed56d30a103e27c8
Yudi <12040397+Yudi@users.noreply.github.com>
2023-12-07 10:40:05 -0300

    Fix apoio anchor

--- src/routes/index-components/nav.svelte
+++ src/routes/index-components/nav.svelte
@@ -13,7 +13,7 @@
       <label tabindex="0" class="btn btn-ghost lg:hidden">
         <Menu class="h-5 w-5" />
       </label>
-      <ul tabindex="0" class="menu dropdown-content rounded-box menu-lg z-[1] mt-3 w-52 bg-base-100 p-2 shadow">
+      <ul tabindex="0" class="menu dropdown-content menu-lg z-[1] mt-3 w-52 rounded-box bg-base-100 p-2 shadow">
         <li><a href="#sobre">Sobre</a></li>
         <li><a href="#programacao">Programação</a></li>
         <li><a href="#duvidas">Dúvidas</a></li>
@@ -26,7 +26,7 @@
         <li><a href="#sobre">Sobre</a></li>
         <li><a href="#programacao">Programação</a></li>
         <li><a href="#duvidas">Dúvidas</a></li>
-        <li><a href="#apoio">Apoio</a></li>
+        <li><a href="#footer">Apoio</a></li>
       </ul>
       <a class="btn">Inscrever-se</a>
     </div>
