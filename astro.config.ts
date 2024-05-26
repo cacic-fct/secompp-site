8ede509f29117bdf476c48638f380d43719e43b3
Yudi <12040397+Yudi@users.noreply.github.com>
2024-05-26 08:41:29 -0300

    Only use shield on production

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 
 // Astro-Shield Configuration
-import { shield } from '@kindspells/astro-shield';
+import { shield, type ShieldOptions } from '@kindspells/astro-shield';
 import { resolve } from 'node:path';
 
 // Generate hashes for external resources
@@ -11,18 +11,28 @@ import { resolve } from 'node:path';
 const rootDir = new URL('.', import.meta.url).pathname;
 const modulePath = resolve(rootDir, 'src', 'generated', 'sriHashes.mjs');
 
-// https://astro.build/config
-export default defineConfig({
-  integrations: [svelte(),    
-    shield({
+const isProd = import.meta.env.PROD;
+
+let shieldConfig: ShieldOptions = {};
+
+if (isProd) {
+  shieldConfig = {
     sri: { hashesModule: modulePath, enableMiddleware: true },
     securityHeaders: {
       contentSecurityPolicy: {
         cspDirectives: {
           'default-src': "'none'",
           'script-src': "'self' https://plausible.fctapp.yudi.me",
-        },
-      },
-    },
-  }),]
+          'img-src': "'self'",
+          'frame-src': 'https://www.google.com/maps/embed/',
+          'font-src': "'self'"
+        }
+      }
+    }
+  };
+}
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [svelte(), shield(shieldConfig)]
 });
