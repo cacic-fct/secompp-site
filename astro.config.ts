3eac0c56b34af287bf1be8c0223157568d72bb7f
Yudi <12040397+Yudi@users.noreply.github.com>
2024-07-02 03:35:04 -0300

    Add sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import svelte from '@astrojs/svelte';
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://secompp.cacic.dev.br',
-  integrations: [svelte()]
+  integrations: [svelte(), sitemap()]
 });
