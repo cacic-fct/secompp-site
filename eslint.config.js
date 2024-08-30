662a1af67a9c9371bf573d594d4e8fea05a4fa0f
Yudi <12040397+Yudi@users.noreply.github.com>
2024-08-30 13:25:35 -0300

    Format files

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,26 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import eslintPluginAstro from "eslint-plugin-astro";
-import eslintPluginSvelte from "eslint-plugin-svelte";
-import svelteParser from "svelte-eslint-parser";
-import tsParser from "@typescript-eslint/parser";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginAstro from 'eslint-plugin-astro';
+import eslintPluginSvelte from 'eslint-plugin-svelte';
+import svelteParser from 'svelte-eslint-parser';
+import tsParser from '@typescript-eslint/parser';
 
 export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
-  ...eslintPluginAstro.configs["jsx-a11y-strict"],
-  ...eslintPluginSvelte.configs["flat/prettier"],
+  ...eslintPluginAstro.configs['jsx-a11y-strict'],
+  ...eslintPluginSvelte.configs['flat/prettier'],
   {
-    files: ["*.astro"],
+    files: ['*.astro'],
     languageOptions: {
-      parser: "astro-eslint-parser",
+      parser: 'astro-eslint-parser',
       globals: globals.browser,
     },
   },
   {
-    files: ["*.svelte"],
+    files: ['*.svelte'],
     languageOptions: {
       parser: svelteParser,
       globals: globals.browser,
@@ -28,5 +28,8 @@ export default [
         parser: tsParser,
       },
     },
+  },
+  {
+    ignores: ['src/env.d.ts'],
   },
 ];
