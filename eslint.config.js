2403f62c25a85cfc330402ba41e2afbfc1948c39
Yudi <12040397+Yudi@users.noreply.github.com>
2024-08-30 13:21:49 -0300

    Setup lint

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginAstro from "eslint-plugin-astro";
+import eslintPluginSvelte from "eslint-plugin-svelte";
+import svelteParser from "svelte-eslint-parser";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  ...eslintPluginAstro.configs["jsx-a11y-strict"],
+  ...eslintPluginSvelte.configs["flat/prettier"],
+  {
+    files: ["*.astro"],
+    languageOptions: {
+      parser: "astro-eslint-parser",
+      globals: globals.browser,
+    },
+  },
+  {
+    files: ["*.svelte"],
+    languageOptions: {
+      parser: svelteParser,
+      globals: globals.browser,
+      parserOptions: {
+        parser: tsParser,
+      },
+    },
+  },
+];
