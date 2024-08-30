f04f6ad98a9628daf1238d39e0714cca8c788eba
Yudi <12040397+Yudi@users.noreply.github.com>
2024-08-30 11:55:02 -0300

    Setup prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,25 @@
+/** @type {import("prettier").Config} */
+export default {
+  singleQuote: true,
+  bracketSameLine: true,
+  printWidth: 120,
+  plugins: [
+    "prettier-plugin-astro",
+    "prettier-plugin-svelte",
+    "prettier-plugin-tailwindcss",
+  ],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+    {
+      files: "*.svelte",
+      options: {
+        parser: "svelte",
+      },
+    },
+  ],
+};
