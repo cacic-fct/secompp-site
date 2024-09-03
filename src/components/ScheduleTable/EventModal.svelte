cca7011840b67e931e699ce929a5af17d6f55c98
Yudi <12040397+Yudi@users.noreply.github.com>
2024-09-03 19:25:15 -0300

    wip

--- src/components/ScheduleTable/EventModal.svelte
+++ src/components/ScheduleTable/EventModal.svelte
@@ -16,8 +16,9 @@
 <dialog class="modal" class:modal-open={isModalOpen}>
   <div class=" modal-box bg-base-200">
     <form method="dialog" style="">
-      <button class="btn btn-circle btn-ghost btn-sm absolute right-2 top-2" on:click={() => toggleModal()}
-        ><X /></button>
+      <button class="btn btn-circle btn-ghost btn-sm absolute right-2 top-2" on:click={() => toggleModal()}>
+        <X />
+      </button>
     </form>
     <h3 class="pb-8 pr-4 text-lg font-bold text-[var(--primary-color)]">{event.eventName} - {type}</h3>
     <EventInfo {event} />
