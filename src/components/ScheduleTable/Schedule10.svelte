f2511f10b01b5c478881156274a7b3a071f3c531
Yudi <12040397+Yudi@users.noreply.github.com>
2024-09-03 13:43:52 -0300

    wip

--- src/components/ScheduleTable/Schedule10.svelte
+++ src/components/ScheduleTable/Schedule10.svelte
@@ -21,7 +21,7 @@
 
 <div class="schedule-container container mx-auto px-4">
   <h1>Programação do Laboratório 10</h1>
-  <span>Recomenda-se trazer o seu próprio computador</span>
+  <span>Recomenda-se levar o seu próprio computador</span>
   <div class="scrolling-touch block w-full overflow-auto pt-8" style="--bs-secondary-rgb: 255, 255, 255">
     <table class="mb-4 w-full max-w-full bg-transparent">
       <thead class="text-left">
