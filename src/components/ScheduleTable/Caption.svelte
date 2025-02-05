5fd36352afb5c46dfcd03565c744c4cc8e27a2fc
Yudi <12040397+Yudi@users.noreply.github.com>
2025-02-05 11:03:32 -0300

    Change ids to classes

--- src/components/ScheduleTable/Caption.svelte
+++ src/components/ScheduleTable/Caption.svelte
@@ -4,7 +4,7 @@
 
 <div class="caption-container text-xs" aria-label="legenda da tabela de horários">
   <div class="flex flex-wrap" style="max-width: 574px;">
-    <div id="icon-desc" class="relative sm:flex-1 sm:flex-grow">
+    <div class="icon-desc relative sm:flex-1 sm:flex-grow">
       <div>
         <img src={minicursosIcon.src} loading="lazy" alt="Ícone minicurso" class="caption-icon" />
         Minicurso
@@ -15,10 +15,10 @@
       </div> -->
     </div>
     <div class="relative sm:flex-1 sm:flex-grow">
-      <p class="caption-gray" id="caption-large-screen">
+      <p class="caption-gray caption-large-screen">
         Clique no evento para visualizar mais informações.<br />
       </p>
-      <div id="caption-small-screen">
+      <div class="caption-small-screen">
         <p class="mb-0">Arraste para a esquerda para visualizar mais eventos.</p>
         <p>
           Clique ou toque no evento para visualizar mais informações.<br />
--- src/components/ScheduleTable/CaptionPalestra.svelte
+++ src/components/ScheduleTable/CaptionPalestra.svelte
@@ -4,17 +4,17 @@
 
 <div class="caption-container text-xs" aria-label="legenda da tabela de horários">
   <div class="flex flex-wrap" style="max-width: 574px;">
-    <div id="icon-desc" class="relative sm:flex-1 sm:flex-grow">
+    <div class="icon-desc relative sm:flex-1 sm:flex-grow">
       <div>
         <img src={palestrasIcon.src} alt="Ícone palestra" loading="lazy" class="caption-icon" />
         Palestra
       </div>
     </div>
     <div class="relative sm:flex-1 sm:flex-grow">
-      <p class="caption-gray" id="caption-large-screen">
+      <p class="caption-gray caption-large-screen">
         Clique no evento para visualizar mais informações.<br />
       </p>
-      <div id="caption-small-screen">
+      <div class="caption-small-screen">
         <p class="mb-0">Arraste para a esquerda para visualizar mais eventos.</p>
         <p>
           Clique ou toque no evento para visualizar mais informações.<br />
