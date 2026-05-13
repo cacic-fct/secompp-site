81c6b8a52506230d413a68f155b028db0f827c3e
Yudi <12040397+Yudi@users.noreply.github.com>
2026-05-13 15:09:02 -0300

    Use fontsource

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import svelte from '@astrojs/svelte';
 
 import sitemap from '@astrojs/sitemap';
@@ -7,4 +7,12 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://secompp.cacic.dev.br',
   integrations: [svelte(), sitemap()],
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: 'Inter',
+      cssVariable: '--font-inter',
+      weights: ['100 900'],
+    },
+  ],
 });
